c77e81c2291e291223a23a6655ca9578ea2c1b44
ganikus1011 <ganikus1011@gmail.com>
2021-11-26 21:09:03 -0500

    Ingreso datos a Firebase

--- app/src/main/java/com/example/nuevo/marco3.java
+++ app/src/main/java/com/example/nuevo/marco3.java
@@ -2,20 +2,27 @@ package com.example.nuevo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class marco3 extends AppCompatActivity {
-
+    private EditText et1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marco3);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        et1 = (EditText) findViewById(R.id.c_entrada);
     }
-    public void ActivityMain(View vie){
+    public void ActivityMain(View view){
         Intent newIntent = new Intent(  this, MainActivity.class);
         startActivity(newIntent);
         newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -30,5 +37,11 @@ public class marco3 extends AppCompatActivity {
         return true;
 
     }
+    public void enviarMensaje(View view){
+        String mens = et1.getText().toString();
+         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Mensaje");
 
+        myRef.setValue(mens);
+    }
 }
